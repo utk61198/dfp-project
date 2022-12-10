07a1ec6bf3d5c43b3647062fa4abe8a3f1705608
utk61198 <utk61198@gmail.com>
2022-12-10 04:13:33 -0500

    team and twitter func

--- dashboard.py
+++ dashboard.py
@@ -1,32 +1,35 @@
 import streamlit as st
 import pandas as pd
 import functions
-
 st.set_page_config(layout='wide')
-
-col1, col2 = st.columns(2)
-
-
+st.snow()
+st.title()
+col1, col2,col3,col4 = st.tabs(["Team","Player","Twitter","Bet odds"])
 with col1:
-    with st.expander("Team"):
-        # team_name = st.text_input("Enter a team name")
-        team_df = pd.read_csv("results.csv")
-        # st.dataframe(pd.read_csv("results.csv"))
-        st.title("Select Team")
-        teams = ['Croatia', 'Senegal', 'Iran', 'Denmark', 'Tunisia', 'Spain', 'Serbia', 'South Korea', 'Brazil', 'Ecuador', 'United States', 'Saudi Arabia', 'Poland', 'Cameroon', 'Australia', 'Mexico', 'Argentina', 'France', 'Qatar', 'Portugal', 'Uruguay', 'Switzerland', 'Germany', 'Costa Rica', 'Morocco', 'Wales', 'Ghana', 'England', 'Netherlands', 'Belgium', 'Japan', 'Canada']
-        team = st.selectbox("Select an option", teams)
-        # col_inner_1, col_inner_2, col_inner_3 = st.columns(3)
-        st.metric("Win", functions.get_win_count(team,team_df))
-        st.metric("Loss", functions.get_loss_count(team,team_df))
-        st.metric("Draw", functions.get_draw_count(team,team_df))
-        last_5_matches = functions.get_last_5_matches(team, team_df)
-        st.title("Last 5 Matches")
-        st.table(last_5_matches[['home_team', 'home_score', 'away_score', 'away_team']].reset_index(drop=True))
-    with st.expander("Twitter"):
-        st.write("twitter")
+    # with st.expander("Team"):
+    team_df = pd.read_csv("results.csv")
+    st.title("Select Team")
+    teams = ['Croatia', 'Senegal', 'Iran', 'Denmark', 'Tunisia', 'Spain', 'Serbia', 'South Korea', 'Brazil', 'Ecuador', 'United States', 'Saudi Arabia', 'Poland', 'Cameroon', 'Australia', 'Mexico', 'Argentina', 'France', 'Qatar', 'Portugal', 'Uruguay', 'Switzerland', 'Germany', 'Costa Rica', 'Morocco', 'Wales', 'Ghana', 'England', 'Netherlands', 'Belgium', 'Japan', 'Canada']
+    team = st.selectbox("Select an option", teams)
+    st.metric("Win", functions.get_win_count(team,team_df))
+    st.metric("Loss", functions.get_loss_count(team,team_df))
+    st.metric("Draw", functions.get_draw_count(team,team_df))
+    last_5_matches = functions.get_last_5_matches(team, team_df)
+    st.title("Last 5 Matches")
+    st.table(last_5_matches[['home_team', 'home_score', 'away_score', 'away_team']].reset_index(drop=True))
 with col2:
-    with st.expander("Player"):
-        st.write("player")
-    
-    with st.expander("Betting odds"):
-        st.write("ods")
+    st.write("Player")
+with col3:
+    teams = ['Croatia', 'Senegal', 'Iran', 'Denmark', 'Tunisia', 'Spain', 'Serbia', 'South Korea', 'Brazil', 'Ecuador', 'United States', 'Saudi Arabia', 'Poland', 'Cameroon', 'Australia', 'Mexico', 'Argentina', 'France', 'Qatar', 'Portugal', 'Uruguay', 'Switzerland', 'Germany', 'Costa Rica', 'Morocco', 'Wales', 'Ghana', 'England', 'Netherlands', 'Belgium', 'Japan', 'Canada']
+    team = st.selectbox("Select a tweet", teams)
+    api = functions.authenticate_tweepy()
+    tweets = api.search_tweets(q=f'"FIFA World Cup" {team}',lang='en',result_type = 'popularity',count=500)
+    for tweet in tweets[:10]:
+        st.write(tweet.text)
+        st.markdown("""---""")
+    sentiment = functions.tweets_sentiment_score(tweets)
+    # st.slider(value=sentiment,label="Tweets Sentiment",disabled=True,min_value=-1.0,max_value=1.0,step=0.01)
+    st.progress(int(sentiment*50+50))
+
+with col4:
+    st.write("Odds")
--- functions.py
+++ functions.py
@@ -1,86 +1,71 @@
 import pandas as pd
+import tweepy
+import re
+import statistics
+from textblob import TextBlob
+
 def get_win_count(team_name, df):
     df['date'] = pd.to_datetime(df['date'])
-
-    # Filter the dataframe to only include matches after 2018
     df = df[df['date'] >= '2018-01-01']
     df = df[df['date'] < '2022-11-20']
-
-    # Get the rows where the specified team was the home team and they won (home_score > away_score)
     home_wins = df[(df['home_team'] == team_name) & (df['home_score'] > df['away_score'])]
-
-    # Get the rows where the specified team was the away team and they won (away_score > home_score)
     away_wins = df[(df['away_team'] == team_name) & (df['away_score'] > df['home_score'])]
-
-    # Sum the number of rows in the home_wins and away_wins dataframes to get the total number of wins
     win_count = len(home_wins) + len(away_wins)
-
-    # Print the win count
     return win_count
 
 def get_draw_count(team_name, matches):
-    # Load the data into a pandas dataframe
     df = pd.DataFrame(matches, columns=['date', 'home_team', 'away_team', 'home_score', 'away_score', 'tournament', 'city', 'country', 'neutral'])
-
-    # Convert the date column to datetime
     df['date'] = pd.to_datetime(df['date'])
-
-    # Filter the dataframe to only include matches after 2018
     df = df[df['date'] >= '2018-01-01']
-
-    # Get the rows where the specified team was the home team and the match was a draw (home_score = away_score)
     home_draws = df[(df['home_team'] == team_name) & (df['home_score'] == df['away_score'])]
-
-    # Get the rows where the specified team was the away team and the match was a draw (away_score = home_score)
     away_draws = df[(df['away_team'] == team_name) & (df['away_score'] == df['home_score'])]
-
-    # Sum the number of rows in the home_draws and away_draws dataframes to get the total number of draws
     draw_count = len(home_draws) + len(away_draws)
-
     return draw_count
 
 
 def get_loss_count(team_name, matches):
-    # Load the data into a pandas dataframe
     df = pd.DataFrame(matches, columns=['date', 'home_team', 'away_team', 'home_score', 'away_score', 'tournament', 'city', 'country', 'neutral'])
-
-    # Convert the date column to datetime
     df['date'] = pd.to_datetime(df['date'])
-
-    # Filter the dataframe to only include matches after 2018
     df = df[df['date'] >= '2018-01-01']
     df = df[df['date'] < '2022-11-20']
-
-
-    # Get the rows where the specified team was the home team and they lost (home_score < away_score)
     home_losses = df[(df['home_team'] == team_name) & (df['home_score'] < df['away_score'])]
-
-    # Get the rows where the specified team was the away team and they lost (away_score < home_score)
     away_losses = df[(df['away_team'] == team_name) & (df['away_score'] < df['home_score'])]
-
-    # Sum the number of rows in the home_losses and away_losses dataframes to get the total number of losses
     loss_count = len(home_losses) + len(away_losses)
     return loss_count
 
 
 def get_last_5_matches(team_name, matches):
-    # Load the data into a pandas dataframe
     df = pd.DataFrame(matches, columns=['date', 'home_team', 'away_team', 'home_score', 'away_score', 'tournament', 'city', 'country', 'neutral'])
-
-    # Convert the date column to datetime
     df['date'] = pd.to_datetime(df['date'])
-
-    # Sort the dataframe by date in descending order
     df = df.sort_values(by='date', ascending=False)
-
-    # Get the rows where the specified team was the home team or the away team
     team_matches = df[(df['home_team'] == team_name) | (df['away_team'] == team_name)]
-
-    # Get the last 5 rows of the team_matches dataframe
     last_5_matches = team_matches.iloc[:5]
     return last_5_matches
-    # return_str = ""
-    # for i,row in last_5_matches.iterrows():
-    #     return_str = return_str + "\n" + f"{row['home_team']} {row['home_score']} - {row['away_score']} {row['away_team']}"
 
-    return return_str
+def authenticate_tweepy():
+    consumer_key="20QCtfjScHSWfzohGhGjYUs7w"
+    consumer_secret="Z3tIfmpILNB4kYJIQ7Q1Omg54Ru90mxsCdTz8Yki69010C2EcG"
+    access_token="1138374206750482433-UnrC2n2zjvpbMQ0cIMlibTKPfB8s1Q"
+    access_token_secret="ExSHUpNT06wYPRL7DvZMIhihWTSpfXiyR633AgQfScCMF"
+    auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+    )
+    api = tweepy.API(auth,wait_on_rate_limit=True)
+    return api
+
+
+def preprocessTweets(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet.text).split())
+
+def tweets_sentiment_score(tweets):
+    # sentiment_intensity_obj = SentimentIntensityAnalyzer()
+    total_sentiment = []
+    for tweet in tweets:
+        # total_sentiment.append(sentiment_intensity_obj.polarity_scores(tweet.text)['pos'])
+        analysis=TextBlob(preprocessTweets(tweet))
+        total_sentiment.append(analysis.sentiment.polarity)
+    print(total_sentiment)
+    total_sentiment = [i for i in total_sentiment if i != 0]
+    return statistics.mean(total_sentiment)
+
+# geosearch
